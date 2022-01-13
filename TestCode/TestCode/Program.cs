4a51088b47e94ffaaaf4a8ae9b75639a5717adb1
DESKTOP-7MJHK46\master <wjswhdgus123@naver.com>
2022-01-13 22:39:14 +0900

    2022_01_13 배열 예제

---
+++ TestCode/TestCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrNum = new string[3] { "한빈", "바보", "안녕" };
+            for( int i =0; i<3; i++)
+            {
+                Console.WriteLine(arrNum[i]);
+            }
+        }
+    }
+}
